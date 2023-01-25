db88b4d1000bc4e61017088c4423710a3417cdfb
Benjamin Drury <47294308+BenjDrury@users.noreply.github.com>
2023-01-25 02:52:19 +0100

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+
+module.exports = {
+  context: __dirname, // to automatically find tsconfig.json
+  entry: "./src/index.ts",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        // add transpileOnly option if you use ts-loader < 9.3.0
+        // options: {
+        //   transpileOnly: true
+        // }
+      },
+    ],
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({ typescript: { memoryLimit: 4096 } }),
+  ],
+  watchOptions: {
+    // for some systems, watching many files can result in a lot of CPU or memory usage
+    // https://webpack.js.org/configuration/watch/#watchoptionsignored
+    // don't use this pattern, if you have a monorepo with linked packages
+    ignored: /node_modules/,
+  },
+};
